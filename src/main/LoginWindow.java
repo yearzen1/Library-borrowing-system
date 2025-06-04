4bec541a693976dd4efbe3e284424908d32c480e
jiahui <1017714519@qq.com>
2025-06-04 11:49:05 +0800

    update

--- src/main/LoginWindow.java
+++ src/main/LoginWindow.java
@@ -1,3 +1,4 @@
+package main;
 import javax.swing.*; 
 import java.awt.*; 
 import java.util.HashMap; 
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,6 @@ package main;
 public class Main {
     public static void main(String[] args) throws Exception{
         Jdatabase.init("root", "123456");//改成自己数据库的密码
-        
+        LoginWindow loginWindow = new LoginWindow();
     }
 }
